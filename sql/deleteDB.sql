95d6768dbb96b21348757106402d102113499fcb
shahmeet8140 <shahmeet8140@gmail.com>
2021-11-13 16:47:29 -0600

    added two files in sql folder

---
+++ sql/deleteDB.sql
@@ -0,0 +1,8 @@
+DROP TABLE current_stock_info;
+DROP TABLE technical_analysis;
+DROP TABLE fundamental_analysis;
+DROP TABLE current_stock_info;
+DROP TABLE accounts;
+DROP TABLE brokerage;
+DROP TABLE login;
+DROP TABLE location;
