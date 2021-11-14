2bf08c7e9b0b4f172cdd3df6b64084450e78a662
shahmeet8140 <77177208+shahmeet8140@users.noreply.github.com>
2021-11-13 18:16:28 -0600

    Update storep_functions.sql
    ‘storep_functions

--- sql/storep_functions.sql
+++ sql/storep_functions.sql
@@ -22,3 +22,48 @@ SELECT MAX(open_price)
     INTO openprice
     FROM current_stock_info$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `avgopen_price`() RETURNS int
+    READS SQL DATA
+BEGIN
+	DECLARE avgopen_price INT;
+    
+    SELECT AVG(open_price)
+    INTO avgopen_price
+    FROM current_stock_info;
+    
+    
+RETURN avgopen_price;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `maxopen_price`() RETURNS int
+    READS SQL DATA
+BEGIN
+	DECLARE maxopen_price INT;
+    
+    SELECT MAX(open_price)
+    INTO maxopen_price
+    FROM current_stock_info;
+    
+    
+RETURN maxopen_price;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `minopen_price`() RETURNS int
+    READS SQL DATA
+BEGIN
+	DECLARE minopen_price INT;
+    
+    SELECT MIN(open_price)
+    INTO minopen_price
+    FROM current_stock_info;
+    
+    
+RETURN minopen_price;
+END$$
+DELIMITER ;
