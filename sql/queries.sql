a679ef5f4743f2718284a0e8a64f3357591f3429
shahmeet8140 <77177208+shahmeet8140@users.noreply.github.com>
2021-12-02 13:49:39 -0600

    simple queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,8 @@
+SELECT * FROM stock_market.location
+WHERE zip = 60111; 
 
+SELECT * FROM stock_market.current_stock_info
+WHERE open_price > 200;
+
+SELECT * FROM stock_market.fundamental_analysis
+WHERE dividend > 2.5;
