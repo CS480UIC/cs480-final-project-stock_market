3bc3db2f6e0f949e23d62c9da88f62412229248f
shahmeet8140 <77177208+shahmeet8140@users.noreply.github.com>
2021-12-02 15:17:38 -0600

    COMPLEX 4 AND 5

--- sql/queries.sql
+++ sql/queries.sql
@@ -8,6 +8,12 @@ SELECT * FROM stock_market.fundamental_analysis
 WHERE dividend > 2.5;
 
 
+
+
+
+
+
+
 USE stock_market;
 SELECT fundamental_analysis.net_income, fundamental_analysis.balance_sheet, current_stock_info.name_stock, current_stock_info.open_price
 FROM fundamental_analysis RIGHT OUTER JOIN current_stock_info
@@ -16,12 +22,31 @@ ON fundamental_analysis.CurrKey = current_stock_info.CurrKey;
 
 USE stock_market;
 SELECT fundamental_analysis.net_income, fundamental_analysis.balance_sheet, current_stock_info.name_stock, current_stock_info.open_price
-FROM fundamental_analysis RIGHT OUTER JOIN current_stock_info
+FROM fundamental_analysis LEFT OUTER JOIN current_stock_info
 ON fundamental_analysis.CurrKey = current_stock_info.CurrKey;
+
 
 USE stock_market;
 SELECT net_income, dividend
 FROM fundamental_analysis F
 WHERE dividend > 
-			(SELECT AVG(dividend)
-		FROM fundamental_analysis);
+	(SELECT AVG(dividend)
+	FROM fundamental_analysis);
+
+
+USE stock_market;
+SELECT net_income, dividend
+FROM fundamental_analysis F
+WHERE dividend < 
+	(SELECT AVG(dividend)
+	FROM fundamental_analysis);
+	
+	
+USE stock_market;
+SELECT net_income, dividend, balance_sheet
+FROM fundamental_analysis f
+WHERE EXISTS
+	(SELECT *
+	FROM fundamental_analysis 
+        WHERE CurrKey = f.CurrKey
+        AND roi > 11);
