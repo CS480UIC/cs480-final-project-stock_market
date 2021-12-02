dabdd37a2cb9f5e27b962a7bad9387197cd53345
shahmeet8140 <77177208+shahmeet8140@users.noreply.github.com>
2021-12-02 14:51:22 -0600

    complex 1

--- sql/queries.sql
+++ sql/queries.sql
@@ -6,3 +6,9 @@ WHERE open_price > 200;
 
 SELECT * FROM stock_market.fundamental_analysis
 WHERE dividend > 2.5;
+
+
+USE stock_market;
+SELECT fundamental_analysis.net_income, fundamental_analysis.balance_sheet, current_stock_info.name_stock, current_stock_info.open_price
+FROM fundamental_analysis RIGHT OUTER JOIN current_stock_info
+ON fundamental_analysis.CurrKey = current_stock_info.CurrKey;
