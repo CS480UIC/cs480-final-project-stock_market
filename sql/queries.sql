4775a6815caac5e7e92631d52eb36376850ccb86
shahmeet8140 <77177208+shahmeet8140@users.noreply.github.com>
2021-12-02 15:01:59 -0600

    complex 3

--- sql/queries.sql
+++ sql/queries.sql
@@ -18,3 +18,10 @@ USE stock_market;
 SELECT fundamental_analysis.net_income, fundamental_analysis.balance_sheet, current_stock_info.name_stock, current_stock_info.open_price
 FROM fundamental_analysis RIGHT OUTER JOIN current_stock_info
 ON fundamental_analysis.CurrKey = current_stock_info.CurrKey;
+
+USE stock_market;
+SELECT net_income, dividend
+FROM fundamental_analysis F
+WHERE dividend > 
+			(SELECT AVG(dividend)
+		FROM fundamental_analysis);
